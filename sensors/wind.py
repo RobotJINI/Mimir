8f9c6562487ebfa483264b81c326a08ad1db5e02
“RobotJINI” <gamache88@gmail.com>
2023-04-11 18:55:51 -0400

    playing with wind sensor

--- sensors/wind.py
+++ sensors/wind.py
@@ -3,23 +3,14 @@ import math
 
 
 class WindSensor(ButtonSensor):
-    def __init__(self, radius_cm=9.0, button=5, precision=4):
+    def __init__(self, button=5, precision=4):
         ButtonSensor.__init__(self, button)
 
         self.wind_speed = -1
-        self._radius_cm = radius_cm
+        self._mph_per_tick_sec = 1.492 * 2
         self._precision = precision
-        self._ms_to_mph = 2.23694
 
     def update(self):
         wind_count, interval_sec = self._get_and_reset()
-        self.wind_speed = self._get_speed(wind_count, interval_sec)
-
-    def _get_speed(self, wind_count, interval_sec):
-        circumference_cm = (2 * math.pi) * self._radius_cm
-        rotations = wind_count
-        dist_meters = (circumference_cm * rotations) / 100
-        speed = dist_meters / interval_sec
-
-        return round(speed * self._ms_to_mph, self._precision)
+        self.wind_speed = (wind_count / interval_sec) * self._mph_per_tick_sec
     
