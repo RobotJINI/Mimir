3445273e023174c333844bd192548fe326dd370e
“RobotJINI” <gamache88@gmail.com>
2023-04-17 10:34:32 -0400

    Adding error handling to veml6070 so it doesnt crash program

--- sensors/veml6070_uv.py
+++ sensors/veml6070_uv.py
@@ -4,11 +4,18 @@ import adafruit_veml6070
 
 class LightSensor:
     def __init__(self):
-        self._veml = adafruit_veml6070.VEML6070(board.I2C())
+        try:
+            self._veml = adafruit_veml6070.VEML6070(board.I2C())
+        except Exception as error:
+            print("Error initializing VEML6070 sensor: ", error)
+            self._veml = None
         
         self.uv = 0
         self.risk_level = 0
     
     def update(self):
-        self.uv = self._veml.uv_raw
-        self.risk_level = self._veml.get_index(self.uv)
+        try:
+            self.uv = self._veml.uv_raw
+            self.risk_level = self._veml.get_index(self.uv)
+        except Exception as error:
+            print("Error updating VEML6070 sensor: ", error)
